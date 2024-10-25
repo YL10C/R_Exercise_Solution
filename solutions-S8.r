29e52a15deadcec9d42dc116d6ebe232107128f5
Yilin Chen <yuyuxiangqian@gmail.com>
2024-10-25 14:22:59 +0100

    formatting

--- solutions-S8.r
+++ solutions-S8.r
@@ -1,8 +1,7 @@
 # 8.1
-grep("<dd>Cause:",raw) -> ii ## check for cause field
+grep("<dd>Cause:", raw) -> ii ## check for cause field
 raw[-ii] ## OK these really are missing - code as NA
-x <- gsub(".*<dd>Cause: ","",raw) ## cut text before actaul cause record
-cause <- gsub("</dd>.*","",x) ## cut text from end of record
+x <- gsub(".*<dd>Cause: ", "", raw) ## cut text before actaul cause record
+cause <- gsub("</dd>.*", "", x) ## cut text from end of record
 cause[-ii] <- NA ## set missings to NA
-mean(tolower(cause)=="shooting",na.rm=TRUE) ## prop shooting
-
+mean(tolower(cause) == "shooting", na.rm = TRUE) ## prop shooting
